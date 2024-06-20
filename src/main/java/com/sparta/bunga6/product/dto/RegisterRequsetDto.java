6cb9df9549e5a86b824fd036868a4fb247928dc5
Wooseok1213 <162704734+Wooseok1213@users.noreply.github.com>
2024-06-20 18:26:02 +0900

    Feat: Product 3Layer 설정 및 상품 등록 서비스 구현  #4

---
+++ src/main/java/com/sparta/bunga6/product/dto/RegisterRequsetDto.java
@@ -0,0 +1,10 @@
+package com.sparta.bunga6.product.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RegisterRequsetDto {
+    String name;
+    int price;
+    int stockQuantity;
+}
--- src/main/java/com/sparta/bunga6/product/service/ProductService.java
+++ src/main/java/com/sparta/bunga6/product/service/ProductService.java
@@ -1,7 +1,27 @@
 package com.sparta.bunga6.product.service;
 
+import com.sparta.bunga6.product.dto.RegisterRequsetDto;
+import com.sparta.bunga6.product.dto.RegisterResponseDto;
+import com.sparta.bunga6.product.entity.Product;
+import com.sparta.bunga6.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+    ProductRepository productRepository;
+
+    public RegisterResponseDto registerProduct(RegisterRequsetDto request) {
+
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        product.setStockQuantity(request.getStockQuantity());
+        Product createProduct = productRepository.save(product);
+        RegisterResponseDto responseDto = new RegisterResponseDto(createProduct);
+        return responseDto;
+    }
 }
