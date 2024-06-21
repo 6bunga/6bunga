08338140c2e2b5235c5e59b0a9284543489f2958
Unho Yeo <yuh001205@gmail.com>
2024-06-21 13:05:25 +0900

    Feature/user (#7)
    * Feat: admin token 추가
    
    * Feat: JWT 사용자 권한 추가
    
    * Refactor: 로그아웃 시 리프레시 토큰 삭제하는 기능 리팩토링
    
    * Feat: 사용자 권한 추가, 비밀번호 변경 기능 추가, 로그아웃 기능 리팩토링
    
    * Feat: UserRoleEnum 추가
    
    * Feat: 최근 비밀번호 히스토리 기능 추가
    
    * Feat: 사용자 권한 기능 추가
    
    * Feat: 사용자 권한 추가
    
    * Refactor: 리프레시 토큰 저장 또는 수정 기능 리팩토링
    
    * Refactor: UserController 리팩토링
    
    * Feat: GlobalExceptionHandler 추가

--- src/main/java/com/sparta/bunga6/user/dto/SignupRequest.java
+++ src/main/java/com/sparta/bunga6/user/dto/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.sparta.bunga6.user.dto;
 
+import com.sparta.bunga6.user.entity.UserRoleEnum;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
